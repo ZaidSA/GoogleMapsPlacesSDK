65ec221ca5f886dc14f937c3c028525291174dcb
ZaidSA <zaid.pathan@solutionanalysts.com>
2018-09-01 15:22:39 +0530

    Other sdks public

---
+++ GoogleMapsPlacesSDK/GoogleMapsPlacesSDK-Framework/GoogleMapsPlacesSDK.swift
@@ -0,0 +1,16 @@
+//
+//  GoogleMapsPlacesSDK.swift
+//  GoogleMapsPlacesSDK-Framework
+//
+//  Created by Zaid Pathan on 01/09/18.
+//  Copyright © 2018 Zaid Pathan. All rights reserved.
+//
+
+import Foundation
+import GoogleMaps
+import GooglePlaces
+import GooglePlacePicker
+
+class GoogleMapsPlaces {
+    
+}
--- GoogleMapsPlacesSDK/GoogleMapsPlacesSDK/AppDelegate.swift
+++ GoogleMapsPlacesSDK/GoogleMapsPlacesSDK/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMapsPlacesSDK_Framework
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        GMSPlacesClient.provideAPIKey("YOUR_API_KEY")
         // Override point for customization after application launch.
         return true
     }
