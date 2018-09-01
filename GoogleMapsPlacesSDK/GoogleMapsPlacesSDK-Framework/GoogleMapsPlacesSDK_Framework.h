99da5df025e099a693536d0add2d9e73ded6d482
ZaidSA <zaid.pathan@solutionanalysts.com>
2018-09-01 12:22:13 +0530

    Frameworks support added.

---
+++ GoogleMapsPlacesSDK/GoogleMapsPlacesSDK-Framework/GoogleMapsPlacesSDK_Framework.h
@@ -0,0 +1,19 @@
+//
+//  GoogleMapsPlacesSDK_Framework.h
+//  GoogleMapsPlacesSDK-Framework
+//
+//  Created by Zaid Pathan on 01/09/18.
+//  Copyright © 2018 Zaid Pathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleMapsPlacesSDK_Framework.
+FOUNDATION_EXPORT double GoogleMapsPlacesSDK_FrameworkVersionNumber;
+
+//! Project version string for GoogleMapsPlacesSDK_Framework.
+FOUNDATION_EXPORT const unsigned char GoogleMapsPlacesSDK_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMapsPlacesSDK_Framework/PublicHeader.h>
+
+
